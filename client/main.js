b5edb6e37842e2eb7acd28a57cb16a728678da3f
viktor <viktor@acklenavenue.com>
2016-04-16 02:40:31 -0600

    separated side and navbar into templates

--- client/main.js
+++ client/main.js
@@ -3,4 +3,6 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 import '../imports/ui/viewAllEmployees/viewAllEmployees.js';
+import '../imports/ui/navbar/navbar.js';
+import '../imports/ui/sidebar/sidebar.js';
 
