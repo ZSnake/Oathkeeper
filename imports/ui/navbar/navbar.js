0a8a408d1d7df83330d24df5bb7cd673da7d90a0
viktor <viktor@acklenavenue.com>
2016-04-24 20:06:52 -0600

    logout working

--- imports/ui/navbar/navbar.js
+++ imports/ui/navbar/navbar.js
@@ -1,9 +1,8 @@
 import { Template } from 'meteor/templating';
- 
 import './navbar.html';
 
 Template.navbar.events({
-    'click a#signoutButton': function(event){
-        console.log('alksdfmlaksmdf');
+    'click #signoutButton'(event){
+        Meteor.logout();
     },
 });
